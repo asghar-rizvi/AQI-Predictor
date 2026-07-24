9c00904dcee86c867656a4b6aae74fd9bbfc48c7
asghar-rizvi <asgharqamberrozvi@gmail.com>
2026-07-24 20:26:10 +0500

    feat: add cors middleware and production config

--- core/config.py
+++ core/config.py
@@ -2,3 +2,10 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ARTIFACT_PATH = os.path.join(BASE_DIR, "artifacts", "models", "aqi_model_v1.joblib")
+
+STREAMLIT_URL = os.environ.get("STREAMLIT_URL", "http://localhost:8501")
+ALLOWED_ORIGINS = [
+    STREAMLIT_URL,
+    "http://localhost:8501", 
+    "http://localhost:3000"  
+]
--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 from api.routes import router
+from core.config import ALLOWED_ORIGINS
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="10Pearls AQI Predictor API",
     description="Advanced Delta-based AQI forecasting API",
     version="1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router, prefix="/api/v1")
